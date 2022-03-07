f43cea30c7397532c1e19ca67c06906f25a2603b
RishikeshMali <rishikeshmali924@gmail.com>
2022-03-07 22:40:56 +0530

    importing React Functional Arrow Component (rfac)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
+import {Footer} from './Components/Footer';
 
 function App() {
   // let name="Rushikesh";
@@ -26,6 +27,7 @@ function App() {
     // </div>
     <>
       <Header />
+      <Footer />
     </>
   );
 }
